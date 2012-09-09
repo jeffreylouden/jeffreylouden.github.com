617b2a284267a9277bfa5f760bcab281e9c13e53
Jeffrey Louden <jeff@jefflouden.com>
2012-09-09 05:11:26 -0500

    pulling from working dev

--- _assets/js/scripts.js
+++ _assets/js/scripts.js
@@ -1,8 +1,26 @@
-$(function($){
-    $("#tweet").tweet({
-        username: "jeffreylouden",
-        avatar_size: 0,
-        count: 1,
-        template: "{text}{time}"
+(function( $, win, doc ) {
+    "use strict";
+    
+    var body            =    $('body'),
+        windowHeight    =    $(win).height(),
+        header          =    $('header');
+
+    function headerSize( headerHeight ) {
+        if (body.width() > 768) {
+            header.css('height', (headerHeight - 90) + 'px');
+        } else {
+            header.css('height', '72px');
+        }
+    }
+        
+    function init() {
+        headerSize(windowHeight);
+    }
+    
+    $(win).resize(function () {
+        headerSize($(this).outerHeight());
     });
-});
+    
+    init();
+    
+})(jQuery, window, document);
