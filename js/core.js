e877a72297d4eb67608d9d5bf995822fbc46a4f3
Jeffrey Louden <jeff@jefflouden.com>
2013-02-21 16:22:02 -0500

    refined modal script to working properly with YT

--- js/core.js
+++ js/core.js
@@ -7,9 +7,11 @@
         FIXED               =   'fixed',
         HIDE                =   'hide',
         HREF                =   'href',
+        IMAGE               =   'image',
         IMG                 =   'img',
         PIXEL               =   'px',
         SHOW                =   'show',
+        VIDEO               =   'video',
 
         // Variable Declarations
         noop            =   function() {},
@@ -20,6 +22,10 @@
         body                =   $('body'),
         htmlbody            =   html.add(body),
 
+        iframeString        =   '<iframe allowfullscreen frameborder="0" width="560" height="350" id="yt-{{{yt-id}}}" src="//www.youtube.com/embed/{{{yt-id}}}/?enablejsapi=1&amp;playerapiid={{{yt-id}}}&amp;wmode=transparent&amp;autohide=0&amp;autoplay=0&amp;fs=1&amp;hd=1&amp;rel=0&amp;showinfo=0&amp;start=&amp;theme=dark&amp;cc_load_policy=0"></iframe>',
+        pattern             =   "{{{yt-id}}}",
+        idReplacer          =   new RegExp(pattern, "g"),
+
         footnote            =   $('.footnote'),
 
         footer              =   $('footer'),
@@ -27,28 +33,51 @@
         partyMode           =   credits.find('i'),
         modalLink           =   $('.modal'),
 		modal,
-		modalImage,
+        modalContent,
+        modalContainer,
+		modalContentType,
 		partyOverlay;
 
 	function createModal(url) {
         body.append('<div id="modal"><a class="close" href="#" title="Close">Close</a></div>');
         modal = $('#modal');
 
-        var img = new win.Image();
+        if (url.indexOf('youtube') > -1) {
 
-        img.onload = function() {
-            modal.prepend(img);
-            modalImage = modal.find(IMG);
-            
-            repositionModalImage();
-            
-            modalImage.click(function(e){
+            var id = url.split('?v=')[1];
+            modal.prepend('<div class="vid-contain"><div class="vid">' + iframeString.replace(idReplacer, id) + '</div></div>');
+
+            modalContentType = VIDEO;
+            modalContent = modal.find('iframe');
+
+            repositionModalContent();
+
+            modalContent.click(function(e){
                 e.stopPropagation();
             });
+            
+        } else {
 
-        };
+            var img = new win.Image();
 
-        img.src = url;
+            img.onload = function() {
+                modal.prepend(img);
+
+                modalContent = modal.find(IMG);
+                modalContentType = IMAGE;
+                
+                repositionModalContent();
+
+                modalContent.click(function(e){
+                    e.stopPropagation();
+                });
+
+            };
+
+            img.src = url;
+        }
+
+        
 
         modal.addClass(SHOW);
         htmlbody.addClass(FIXED);
@@ -62,19 +91,23 @@
         });
     }
 
-    function repositionModalImage() {
-        if (!modalImage) {
-            modalImage = modal.find(IMG);
+    function repositionModalContent() {
+
+        if (!modalContent) {
+            modalContent = (modalContentType === VIDEO) ? modal.find('iframe') : modal.find(IMG);
         }
-        if (modalImage.height() < wndw.height()) {
-            modalImage.addClass(CENTERED);
-            modalImage.css({
-                marginTop : '-' + (modalImage.height() / 2) + PIXEL,
-                marginLeft : '-' + (modalImage.width() / 2) + PIXEL
+
+        modalContainer = (modalContentType === VIDEO) ? modalContent.closest('.vid-contain') : modal.find(IMG);
+
+        if (modalContent.innerHeight() < wndw.height()) {
+            modalContainer.addClass(CENTERED);
+            modalContainer.css({
+                marginTop : '-' + Math.floor(modalContent.innerHeight() / 2) + PIXEL,
+                marginLeft : '-' + Math.floor(modalContent.outerWidth() / 2) + PIXEL
             });
         } else {
-            modalImage.removeClass(CENTERED);
-            modalImage.css({
+            modalContainer.removeClass(CENTERED);
+            modalContainer.css({
                 marginTop : '0px',
                 marginLeft : 'auto'
             });
@@ -91,7 +124,7 @@
                 modal.remove();
             }, 200);
             
-            wndw.off('resize', repositionModalImage);
+            wndw.off('resize', repositionModalContent);
         }
     }
 
@@ -100,12 +133,12 @@
             t       =   $(this),
             href    =   t.attr(HREF);
         e.preventDefault();
-        htmlbody.animate({scrollTop: ($(href).offset().top - 100)}, 300);
+        htmlbody.animate({scrollTop: ($(href).offset().top - 75)}, 300);
     });
 
     modalLink.on('click', function(e) {
         e.preventDefault();
-        createModal($(this).attr('data-url'));
+        createModal($(this).attr(HREF));
     });
 
     $(doc.documentElement).keyup(function (event) {
@@ -115,8 +148,8 @@
     });
 
     wndw.resize(function() {
-        if (modalImage) {
-            repositionModalImage();
+        if (modalContent) {
+            repositionModalContent();
         }
     });
 
