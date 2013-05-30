b6f8e9537232546c6a33906c77476388c5a7d771
Jeffrey Louden <jeff@jefflouden.com>
2013-05-30 10:38:15 -0700

    added lazyloading

--- js/core.js
+++ js/core.js
@@ -1,3 +1,5 @@
+;(function($){$.fn.unveil=function(threshold){var $w=$(window),th=threshold||0,retina=window.devicePixelRatio>1,attrib=retina?"data-src-retina":"data-src",images=this,loaded,inview,source;this.one("unveil",function(){source=this.getAttribute(attrib);source=source||this.getAttribute("data-src");if(source)this.setAttribute("src",source);});function unveil(){inview=images.filter(function(){var $e=$(this),wt=$w.scrollTop(),wb=wt+$w.height(),et=$e.offset().top,eb=et+$e.height();return eb>=wt-th&&et<=wb+th;});loaded=inview.trigger("unveil");images=images.not(loaded);}$w.scroll(unveil);$w.resize(unveil);unveil();return this;};})(jQuery);
+
 (function( $, win, doc ) {
     "use strict";
 
@@ -135,5 +137,7 @@
     if (window.location.href.indexOf('party-mode') > -1) {
         partyMode.click();
     }
+
+    $(IMG).unveil(100);
 
 })(jQuery, window, document);
