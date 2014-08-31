6f78eb496aa6f167f50cb87abf601cd86c5fd3e1
Jeffrey Louden <jeff@jefflouden.com>
2014-08-30 21:55:06 -0700

    updating the right file. derp.

--- js/core.js
+++ js/core.js
@@ -1,145 +1,47 @@
-;(function($){$.fn.unveil=function(threshold){var $w=$(window),th=threshold||0,retina=window.devicePixelRatio>1,attrib=retina?"data-src-retina":"data-src",images=this,loaded,inview,source;this.one("unveil",function(){source=this.getAttribute(attrib);source=source||this.getAttribute("data-src");if(source)this.setAttribute("src",source);});function unveil(){inview=images.filter(function(){var $e=$(this),wt=$w.scrollTop(),wb=wt+$w.height(),et=$e.offset().top,eb=et+$e.height();return eb>=wt-th&&et<=wb+th;});loaded=inview.trigger("unveil");images=images.not(loaded);}$w.scroll(unveil);$w.resize(unveil);unveil();return this;};})(jQuery);
-
-(function( $, win, doc ) {
+(function( win, doc ) {
     "use strict";
 
-	var
-		// CONSTANT Declarations
-        ANIMATE             =   'animate',
-        CENTERED            =   'centered',
-        FIXED               =   'fixed',
-        HIDE                =   'hide',
-        HREF                =   'href',
-        ID                  =   'id',
-        IMAGE               =   'image',
-        IMG                 =   'img',
-        PIXEL               =   'px',
-        SHOW                =   'show',
-        VIDEO               =   'video',
+    var
+        speeds = ['fast', 'medium', 'slow']
+        
 
-        // Variable Declarations
-        noop            =   function() {},
-        console         =    win.console || {"log": noop, "warn": noop, "error": noop},
-
-        wndw                =   $(win),
-        html                =   $(doc.getElementsByTagName('html')),
-        body                =   $(doc.getElementsByTagName('body')),
-        htmlbody            =   html.add(body),
-
-        iframeString        =   '<iframe allowfullscreen frameborder="0" width="560" height="350" id="yt-{{{yt-id}}}" src="//www.youtube.com/embed/{{{yt-id}}}/?enablejsapi=1&amp;playerapiid={{{yt-id}}}&amp;wmode=transparent&amp;autohide=0&amp;autoplay=0&amp;fs=1&amp;hd=1&amp;rel=0&amp;showinfo=0&amp;start=&amp;theme=dark&amp;cc_load_policy=0"></iframe>',
-        pattern             =   "{{{yt-id}}}",
-        idReplacer          =   new RegExp(pattern, "g"),
-
-        footnote            =   $('.footnote'),
-
-        footer              =   $(doc.getElementsByTagName('footer')),
-        credits             =   $(doc.getElementById('credits')),
-        partyMode           =   credits.find('i'),
-		partyOverlay        =   $(doc.getElementById('party-mode')),
-
-        modalLink           =   $('.modal'),
-        modal,
-        modalContent,
-        modalContainer,
-        modalContentType,
-
-        Modal = {
-            create: function(url) {
-                if (url.indexOf('youtube') > -1) {
-
-                    var id = url.split('?v=')[1];
-                    modal.prepend('<div class="vid-contain"><div class="vid">' + iframeString.replace(idReplacer, id) + '</div></div>');
-
-                    modalContentType = VIDEO;
-                    modalContent = modal.find('iframe');
-
-                    modalContent.click(function(e){
-                        e.stopPropagation();
-                    });
-
-                } else {
-
-                    var img = new win.Image();
-
-                    img.onload = function() {
-                        modal.prepend(img);
-
-                        modalContent = modal.find(IMG);
-                        modalContentType = IMAGE;
-
-                        modalContent.click(function(e){
-                            e.stopPropagation();
-                        });
-
-                    };
-
-                    img.src = url;
-                }
-
-                modal.removeClass(HIDE).addClass(SHOW);
-                htmlbody.addClass(FIXED);
-
-                modal.find('.close').click(function(e) {
-                    e.preventDefault();
-                });
-
-                modal.click(function() {
-                    Modal.dismiss();
-                });
-            },
-            dismiss: function() {
-                if (modal.length > 0) {
-
-                    htmlbody.removeClass(FIXED);
-                    modal.removeClass(SHOW).addClass(HIDE);
-
-                    setTimeout(function() {
-                        document.getElementById('modal').innerHTML = "";
-                    }, 200);
-                }
-            }
-        };
-
-    if ( modalLink.length ) {
-        body.append('<div id="modal"><a class="close" href="#" title="Close">Close</a></div>');
-        modal = $('#modal');
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    footnote.on('click', function(e) {
+    function createStars(min, max) {
         var
-            t       =   $(this),
-            href    =   t.attr(HREF);
-        e.preventDefault();
-        htmlbody.animate({scrollTop: ($(href).offset().top - 75)}, 300);
-    });
+            starCount = getRandomInt(min, max),
+            starContainer = doc.createElement("div"),
+            fragment = doc.createDocumentFragment();
 
-    modalLink.on('click', function(e) {
-        e.preventDefault();
-        Modal.create($(this).attr(HREF));
-    });
+        for ( var e = 0; e < starCount; e++ ) {
+            var
+                b = doc.createElement("b"),
+                t = getRandomInt(0, window.innerHeight),
+                l = getRandomInt(0, window.innerWidth);
 
-    partyMode.click(function() {
-        htmlbody.addClass(FIXED);
-        partyOverlay.addClass(ANIMATE).addClass(SHOW);
+            b.setAttribute('style', "position:absolute;width:1px;height:1px;top:"+t+"px;left:"+l+"px;");
+            b.setAttribute('class', speeds[getRandomInt(0, 2)]);
 
-        partyOverlay.on('click', function() {
-            $(this).removeClass(SHOW);
-            htmlbody.removeClass(FIXED);
-        });
-    });
-
-    $(doc.documentElement).keyup(function (event) {
-        if (event.keyCode === 27) {
-            Modal.dismiss();
-            partyOverlay.removeClass(SHOW);
-            htmlbody.removeClass(FIXED);
+            fragment.appendChild( b );
         }
-    });
 
-    if (window.location.href.indexOf('party-mode') > -1) {
-        partyMode.click();
+        starContainer.appendChild( fragment.cloneNode(true) )
+         
+        doc.body.appendChild( starContainer.cloneNode(true) );
+        
     }
 
-    $(IMG).unveil(100);
+    window.addEventListener("resize", function() {
+        if (doc.getElementsByTagName('div')) {
+            doc.body.removeChild(doc.getElementsByTagName('div')[0])
+        }
+        createStars(500, 1000);
+    });
 
-})(jQuery, window, document);
+    window.onload = function() {
+        createStars(500, 1000);
+    };
+
+})(window, document);
