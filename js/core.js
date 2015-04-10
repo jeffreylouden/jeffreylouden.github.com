fca96c90b5a3419d9ec39c8116af78619ee4005c
Jeffrey Louden <jeff@jefflouden.com>
2015-04-09 23:41:03 -0700

    made new animation

--- js/core.js
+++ js/core.js
@@ -1,25 +1,28 @@
 (function( win, doc ) {
     "use strict";
 
+    var mouse = {x: 0, y: 0};
+
     var canvas = document.getElementById('lines');
     var context = canvas.getContext('2d');
 
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-        
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    function createLines() {
-        for ( var e = 0; e < window.innerHeight; e++ ) {
-            if (e % 2 === 1) { continue; }
+    function createLines(x,y) {
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+        x = x || 0;
+        y = y || 0;
+        context.clearRect ( 0 , 0 , canvas.width, canvas.height );
+        for ( var e = 0; e < (canvas.width/100); e++ ) {
             var 
-                topOffset = e + .5, // to create single pixel line
-                l = getRandomInt(0, window.innerWidth);
+                l = getRandomInt(0, window.innerWidth),
+                w = getRandomInt(0, window.innerWidth);
             context.beginPath();
-            context.moveTo(l, topOffset);
-            context.lineTo(canvas.width, topOffset);
+            context.moveTo(x, y);
+            context.lineTo(l, w);
             context.strokeStyle = "#"+("000"+(Math.random()*(1<<24)|0).toString(16)).substr(-6);
             context.lineWidth = 1;
             context.stroke();
@@ -33,5 +36,11 @@
     window.onload = function() {
         createLines();
     };
+
+    document.addEventListener('mousemove', function(e){ 
+        mouse.x = e.clientX || e.pageX; 
+        mouse.y = e.clientY || e.pageY 
+        createLines(mouse.x,mouse.y);
+    }, false);
 
 })(window, document);
