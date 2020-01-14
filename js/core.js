4cdb90b528e1d2f627c62d6ad612f3cd82df4901
Jeffrey Louden <jeff@jefflouden.com>
2020-01-14 12:08:47 -0800

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,38 +1,44 @@
 (function( win, doc ) {
     "use strict";
 
+    var canvas = document.getElementById('lines');
+
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     function createLines() {
-        var canvas = document.getElementById('lines');
-
         if (!canvas) { return; }
 
-        var winHeight = window.innerHeight;
-        var winWidth = window.innerWidth;
-
-        var context = canvas.getContext('2d');
+        var
+            winHeight = window.innerHeight,
+            winWidth = window.innerWidth,
+            context = canvas.getContext('2d'),
+            x = getRandomInt(0, winWidth),
+            y = getRandomInt(0, winHeight);
 
         canvas.height = winHeight * 2;
         canvas.width = winWidth * 2;
 
-        context.clearRect (0 , 0 , canvas.width, canvas.height);
-
+        context.clearRect ( 0 , 0 , canvas.width, canvas.height );
+        
         canvas.style.height = winHeight + 'px';
         canvas.style.width = winWidth + 'px';
+        
+        context.scale(2,2);
 
-        context.scale(4,4);
-
-        for (var e = 0, len = ((winWidth + winHeight)*4); e < len; e++) {
-            var
-                x = getRandomInt(0, winWidth),
-                y = getRandomInt(0, winHeight),
+        for ( var e = 0, len = (winWidth/100); e < len; e++ ) {
+            var 
+                randomWidth = getRandomInt(0, winWidth),
+                randomHeight = getRandomInt(0, winHeight),
                 randomColor = "#"+("000"+(Math.random()*(1<<24)|0).toString(16)).substr(-6);
 
-            context.fillStyle = randomColor;
-            context.fillRect(x,y,2,2);
+            context.beginPath();
+            context.moveTo(x, y);
+            context.lineTo(randomWidth, randomHeight);
+            context.strokeStyle = randomColor;
+            context.lineWidth = 1;
+            context.stroke();
         }
     }
 
