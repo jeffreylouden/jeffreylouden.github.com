9b6c8b42796e0762a485ff175af420f54d18e6e0
Jeff Louden <jeff@jefflouden.com>
2020-08-29 16:39:09 -0700

    Simplify structure

--- js/core.js
+++ js/core.js
@@ -27,16 +27,18 @@
         for ( let e = 0, len = winHeight; e < len; e++ ) {
             for (let f = 0, len = winWidth; f < len; f++) {
                 let randomColor = getRandomInt(0, 1);
-                // let randomSkip = getRandomInt(0, 1);
                 
                 // Skip setting color since it would be white anyway                
-                // Random skip to reduce concentration of fill              
                 if (!randomColor) continue;
                     
-                context.fillStyle = 'rbg(0,0,0)';
                 context.fillRect(e, f, getRandomInt(0, 1), getRandomInt(0, 1));
             }
         }
+
+        const main = document.getElementById('main');
+        main.style.top = `${getRandomInt(0, (winHeight / 2))}px`;
+        main.style.left = `${getRandomInt(0, (winWidth / 2))}px`;
+        main.style.opacity = '1';
     }
 
     window.addEventListener("resize", function() {
@@ -45,6 +47,8 @@
 
     window.onload = function() {
         createNoise();
+
+        
     };
 
 })(window, document);
