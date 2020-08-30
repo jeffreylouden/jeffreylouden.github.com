36405b35cc502ff7b9fd915df5f7ce727a918b25
Jeff Louden <jeff@jefflouden.com>
2020-08-29 20:41:19 -0700

    Do it live

--- js/core.js
+++ js/core.js
@@ -1,54 +1,54 @@
-(function( win, doc ) {
-    "use strict";
+(function (win, doc) {
+  "use strict";
 
-    let canvas = document.getElementById('lines');
+  let canvas = document.getElementById("lines");
 
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function createNoise() {
+    if (!canvas) {
+      return;
     }
 
-    function createNoise() {
-        if (!canvas) { return; }
+    let winHeight = window.innerHeight;
+    let winWidth = window.innerWidth;
+    let context = canvas.getContext("2d");
 
-        let winHeight = window.innerHeight;
-        let winWidth = window.innerWidth;
-        let context = canvas.getContext('2d');
+    canvas.height = winHeight;
+    canvas.width = winWidth;
 
-        canvas.height = winHeight * 2;
-        canvas.width = winWidth * 2;
+    context.clearRect(0, 0, canvas.width, winHeight);
 
-        context.clearRect(0, 0, canvas.width, winHeight );
-        
-        canvas.style.height = winHeight+ 'px';
-        canvas.style.width = winWidth + 'px';
-        
-        context.scale(4,4);
+    canvas.style.height = winHeight + "px";
+    canvas.style.width = winWidth + "px";
 
-        for ( let e = 0, len = winHeight; e < len; e++ ) {
-            for (let f = 0, len = winWidth; f < len; f++) {
-                let randomColor = getRandomInt(0, 1);
-                
-                // Skip setting color since it would be white anyway                
-                if (!randomColor) continue;
-                    
-                context.fillRect(e, f, getRandomInt(0, 1), getRandomInt(0, 1));
-            }
-        }
+    context.scale(2, 2);
 
-        const main = document.getElementById('main');
-        main.style.top = `${getRandomInt(0, (winHeight / 2))}px`;
-        main.style.left = `${getRandomInt(0, (winWidth / 2))}px`;
-        main.style.opacity = '1';
+    for (let e = 0, len = winHeight; e < len; e++) {
+      for (let f = 0, len = winWidth; f < len; f++) {
+        let randomColor = getRandomInt(0, 1);
+
+        // Skip setting color since it would be white anyway
+        if (!randomColor) continue;
+
+        context.fillRect(e, f, getRandomInt(0, 1), getRandomInt(0, 1));
+      }
     }
 
-    window.addEventListener("resize", function() {
-        createNoise();
-    });
+    if (winWidth > 400) {
+      const main = document.getElementById("main");
+      main.style.top = `${winHeight / 2 - 200}px`;
+      main.style.opacity = "1";
+    }
+  }
 
-    window.onload = function() {
-        createNoise();
+  window.addEventListener("resize", function () {
+    createNoise();
+  });
 
-        
-    };
-
+  window.onload = function () {
+    createNoise();
+  };
 })(window, document);
